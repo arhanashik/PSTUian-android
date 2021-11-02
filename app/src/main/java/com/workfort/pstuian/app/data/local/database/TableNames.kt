6da2393d52e78449fe3b52297869bbe7d8ff054b
arhanashik <ashik.pstu.cse@gmail.com>
2021-11-03 02:56:13 +0900

    fix : some java files transformed into kotlin

---
+++ app/src/main/java/com/workfort/pstuian/app/data/local/database/TableNames.kt
@@ -0,0 +1,12 @@
+package com.workfort.pstuian.app.data.local.database
+
+object TableNames {
+    const val SLIDER = "slider"
+    const val FACULTY = "faculty"
+    const val STUDENT = "student"
+    const val TEACHER = "teacher"
+    const val BATCH = "batch"
+    const val EMPLOYEE = "employee"
+    const val COURSE_SCHEDULE = "course_schedule"
+    const val CONFIG = "config"
+}
